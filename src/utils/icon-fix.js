35913b97c24990c90296141a624d933ee79dd4bf
zoeyvo <zoeyvo256@gmail.com>
2025-02-05 23:31:09 -0800

    Debugging connecting to react app

---
+++ src/utils/icon-fix.js
@@ -0,0 +1,12 @@
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon; 
