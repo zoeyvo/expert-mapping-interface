d4627bf9f8e9e56f8e5db70de1adbd59174462a9
Alyssa Vallejo <92827768+AV-CompSci-Mage@users.noreply.github.com>
2025-02-13 00:40:51 -0800

    added navbar, basic links, hero image

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import 'leaflet/dist/leaflet.css';
 import './styles/icon-fix';
 import './styles/index.css';
 
+const bgImage = require('./topImage.png');
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold text-center my-4">
-        Geo Data Visualizer
-      </h1>
+      {/* Navbar */}
+        <nav className="flex justify-between items-center py-10 px-12 bg-blue-900 shadow-md">
+          <div className="flex space-x-10">
+            <a href="#" className="text-3xl font-semibold text-white">Aggie Experts Interactive Map</a>
+            <a href="https://experts.ucdavis.edu/browse/expert/a" className="text-3xl font-semibold text-white">Experts</a>
+            <a href="https://experts.ucdavis.edu/browse/grant/1" className="text-3xl font-semibold text-white">Grants</a>
+          </div>
+          <a href="https://www.ucdavis.edu/" className="text-3xl font-semibold text-white">My Account</a>
+        </nav>
+
+        {/* Background Image - hero */}
+       <div 
+         className="w-full min-h-[200px] text-white text-center bg-cover bg-center"
+         style={{ backgroundImage: `url(${bgImage})` }}
+       >       </div>
+
+       {/* Map Section */}
       <div className="container mx-auto p-4">
         <MapContainer
           center={[0, 0]}
           zoom={2}
-          style={{ height: '500px', width: '100%' }}
+          style={{ height: '700px', width: '100%' }}
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
