f4dc864a1cbd0fff24f2c3046c828394f71f2e7c
Alyssa Vallejo <92827768+AV-CompSci-Mage@users.noreply.github.com>
2025-02-17 17:39:05 -0800

    not much we can do with this API as it stands

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,10 +17,53 @@ class MapComponent extends LitElement {
             --leaflet-map-min-height: 63vh; 
         }
     `;
-
+// const latitude = 47.3902;
+// const longitude = 8.5158;
+// const radius = 0;
+// const defaultZoom = 2;
+// const points = [
+// {
+//     id: 3284823890,
+//     latitude: 47.3901151,
+//     longitude: 8.5151409,
+//     title: "Villaggio",
+//     url: "https://www.google.com",
+//   },
+//   {
+//     id: 651504006,
+//     latitude: 47.3902919,
+//     longitude: 8.5172402,
+//     title: "Angkor",
+//     url: "https://www.google.com",
+//   },
+//   {
+//     id: 3284823880,
+//     latitude: 47.3905062,
+//     longitude: 8.5174073,
+//   },
+//   {
+//     id: 3297129090,
+//     latitude: 47.3891608,
+//     longitude: 8.5166309,
+//     title: "novotelcafe",
+//   },
+//   {
+//     id: 305586751,
+//     latitude: 47.3900555,
+//     longitude: 8.5179098,
+//     title: "Toscano im Puls 5",
+//   },
+//   {
+//     id: 2069791719,
+//     latitude: 47.3891509,
+//     longitude: 8.5177829,
+//     title: "Aubrey",
+//     url: "https://www.google.com",
+//   },
+// ];
     render() {
         return html`
-            <leaflet-map latitude="37.7749" longitude="-122.4194" defaultZoom="2"></leaflet-map>
+        <leaflet-map latitude="37.7749" longitude="-122.4194" defaultZoom="2" ></leaflet-map>
         `;
     }
 }
