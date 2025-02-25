065f9731bd8fe8ed640f4b94801ef4669a715f16
Alyssa Vallejo <92827768+AV-CompSci-Mage@users.noreply.github.com>
2025-02-25 00:57:10 -0800

    started dynamic clustering

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,13 @@ import "leaflet/dist/leaflet.css";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+/*
+run `npm install leaflet.markercluster --legacy-peer-deps`
+
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+*/
+
 const customMarker = new L.Icon({
     iconUrl: markerIcon,
     shadowUrl: markerShadow,
@@ -27,6 +34,30 @@ const ResearchMap = () => {
       .catch((error) => console.error("Error loading GeoJSON:", error));
   }, []);
 
+  /*useEffect(() => {
+    if (geoData) {
+      const map = L.map("map").setView([20, 0], 2);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+      }).addTo(map);
+
+      const markers = L.markerClusterGroup();
+
+      geoData.features.forEach((feature) => {
+        const [lng, lat] = feature.geometry.coordinates;
+        const marker = L.marker([lat, lng], { icon: customMarker }).bindPopup(
+          `<strong>${feature.properties.researcher}</strong><br><strong>Related Works:</strong><br>${feature.properties.works?.[0]}<br>${feature.properties.url ? `<a href="${feature.properties.url}" target="_blank" rel="noopener noreferrer">Profile</a>` : ""}`
+           );
+        markers.addLayer(marker);
+      });
+
+      map.addLayer(markers);
+    }
+  }, [geoData]);
+
+  return <div id="map" style={{ height: "50vh", width: "100%" }}></div>;
+};    */
   return (
     <MapContainer center={[20, 0]} zoom={2} style={{ height: "100%", width: "100%" }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; OpenStreetMap contributors' />
